f8a99d22e06f0d90fdca514452ec75e3b4b21ebb
codebyhannah <firekatburns@gmail.com>
2024-02-10 23:53:48 -0700

    added directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,33 @@
+const baseUrl = "https://codebyhannah.github.io/wdd230/";
+const linksUrl = "https://codebyhannah.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers() {
+    const response = await fetch(linksUrl);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+const linksList = document.querySelector(".learningActs")
+
+function displayLinks(data) {
+    data.weeks.forEach(week => {
+        let listWeek = document.createElement("li");
+        let weekNum = document.createElement("span");
+        weekNum.innerText = `${week.week}: `;
+        listWeek.appendChild(weekNum);
+        week.links.forEach(link => {
+            const aLink = document.createElement("a");
+            aLink.setAttribute("href", link.url);
+            aLink.setAttribute("target", "_blank");
+            aLink.innerText = link.title;
+            listWeek.appendChild(aLink);
+            if (week.links.indexOf(link) != week.links.length-1)
+            {
+                listWeek.innerHTML += " | ";
+            }
+        });
+        linksList.appendChild(listWeek);
+    });
+}
+
+getLinks();
